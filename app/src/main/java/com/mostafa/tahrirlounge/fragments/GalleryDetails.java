01cd867e5cefd603c9511786cc104679fdf6c4a7
MostafaKhaled2017 <mostafakhaled835@outlook.com>
2018-01-23 16:02:57 +0200

    Update

--- app/src/main/java/com/mostafa/tahrirlounge/fragments/GalleryDetails.java
+++ app/src/main/java/com/mostafa/tahrirlounge/fragments/GalleryDetails.java
@@ -27,20 +27,20 @@ import java.util.ArrayList;
 
 public class GalleryDetails extends Fragment {
     View myView;
-    ImageView mainImage;
+   // ImageView mainImage;
     RecyclerView galleryDetailsRecyclerView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.fragment_gallery_in_details, container, false);
-        mainImage= (ImageView) myView.findViewById(R.id.main_image_in_gallery_details);
+      //  mainImage= (ImageView) myView.findViewById(R.id.main_image_in_gallery_details);
         galleryDetailsRecyclerView = (RecyclerView) myView.findViewById(R.id.gallery_details_recycler_view);
         galleryDetailsRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));//TODO :autofit the number of col.
         Bundle galleryData = this.getArguments();
         Log.v("galleryDataLogging","gallery data bundle is : "+galleryData.toString());
         ArrayList<String> galleryImages=galleryData.getStringArrayList("album");
         Log.v("galleryDataLogging","gallery images is : "+galleryImages.toString());
-        Glide.with(getActivity()).load(galleryImages.get(0)).placeholder(R.drawable.image_background).into(mainImage);
+       // Glide.with(getActivity()).load(galleryImages.get(0)).placeholder(R.drawable.image_background).into(mainImage);
         GalleryDetailsAdapter adapter = new GalleryDetailsAdapter(getActivity(),galleryImages);
         galleryDetailsRecyclerView.setAdapter(adapter);
         return myView;
